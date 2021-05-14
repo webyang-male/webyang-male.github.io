b1763c279b152a814b7b92597b6104d2d1390dda
webyang-male <mr.yangweb@gmail.com>
2021-05-14 20:40:47 +0800

    blog update commit

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>console.log(
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer@latest/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer@latest/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="https://cdn.jsdelivr.net/npm/meting@1/dist/Meting.min.js"></script>console.log(
   "%c婷宝儿我送你归于人海了,祝你幸福🍀",
   "border:2px solid deepskyblue;color:lightpink;font-size:16px;font-weight:900;"
 );
